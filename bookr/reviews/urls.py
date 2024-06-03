d73d86b2f9f18afa4760706e3b721719b4171693
binoddhamala <binoddhamla969@gmail.com>
2024-06-03 17:14:33 +0545

    book_details added

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -1,4 +1,5 @@
 
 from django.urls import path
 from . import views
-urlpatterns = [path('books/', views.book_list, name='book_list'),]
+urlpatterns = [path('books/', views.book_list, name='book_list'),
+               path('book/<int:pk>/', views.book_detail,name='book_detail')]
--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Review
 from .utils import average_rating
+from django.shortcuts import render, get_object_or_404
 
 
 def book_list(request):
@@ -22,3 +23,17 @@ def book_list(request):
         'book_list': book_list
     }
     return render(request, 'reviews/books_list.html', context)
+
+def book_detail(request,pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+
+    if reviews:
+    
+        book_rating = average_rating([review.rating for review in reviews])
+        context = {"book": book,"book_rating": book_rating,"reviews": reviews}
+
+    else:
+        context = {"book": book,"book_rating": None,"reviews": None}
+        
+        return render(request, "reviews/book_detail.html", context)
