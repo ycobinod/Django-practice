d34af05297a27a106dd41479c30452418594d0ac
binoddhamala <binoddhamla969@gmail.com>
2024-06-07 17:18:54 +0545

    practice cbv

--- bookr/book_management/views.py
+++ bookr/book_management/views.py
@@ -1,22 +1,24 @@
 from django.http import HttpResponse
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 from django.views import View
 from .forms import BookForm
+from .models import Book
 
 class BookRecordFormView(FormView):
-    template_name='book_form.html'
-    form_class=BookForm
-    success_url='book_management/entry_success'
-
+    template_name = 'book_form.html'
+    form_class = BookForm
+    success_url = '/book_management/entry_success' 
 
     def form_valid(self, form):
         form.save()
-        return super() .form_valid(form)
-    
+        return super().form_valid(form)
+
 class FormSuccessView(View):
-    def get(self):
-        return HttpResponse("Book record save successfully")
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Book record saved successfully")
     
-
-
-
+class BookCreateView(CreateView):
+ model=Book
+ fields= ['name','author']
+ template_name='book_form.html'
+ success_url='/book_management/entry_success'
--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -1,7 +1,8 @@
 from reviews.admin import admin_site
-from django.urls import include,path
+from django.urls import include, path
 
-urlpatterns = [path('admin/', admin_site.urls),
-               path('', include('reviews.urls')),
-               path('book_management/',include('book_management.urls'))
+urlpatterns = [
+    path('admin/', admin_site.urls),
+    path('', include('reviews.urls')),
+    path('book_management/', include('book_management.urls'))
 ]
